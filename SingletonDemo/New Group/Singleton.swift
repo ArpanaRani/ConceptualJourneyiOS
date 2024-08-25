d2d31216da4c70cbc1228ac627e474f15ba7244c
Arpana <arpana.rani@zopsmart.com>
2024-08-25 10:11:22 +0530

    dscd

---
+++ SingletonDemo/New Group/Singleton.swift
@@ -0,0 +1,17 @@
+//
+//  ApiClient.swift
+//  SingletonDemo
+//
+//  Created by Arpana on 11/08/24.
+//
+
+import Foundation
+
+class Singleton {
+    
+    static let sharedInstance
+    
+    private init {
+    
+    }
+}
